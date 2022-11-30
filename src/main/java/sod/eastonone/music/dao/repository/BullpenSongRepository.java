5532a264a8876ef53218782d1ac568953aca00d7
dbrown725 <davidbrown772255@gmail.com>
2022-11-30 15:44:19 -0500

    Added logic to set the sort order correctly on the first song ever added to the BP for a particular user.

--- src/main/java/sod/eastonone/music/dao/repository/BullpenSongRepository.java
+++ src/main/java/sod/eastonone/music/dao/repository/BullpenSongRepository.java
@@ -9,6 +9,9 @@ import sod.eastonone.music.dao.entity.BullpenSong;
 @Repository
 public interface BullpenSongRepository extends JpaRepository<BullpenSong, Integer> {
 	
+	@Query(value="SELECT count(*) FROM bullpen_song WHERE user_id = ?1", nativeQuery=true)
+	public int getSongCountByUserId(int userId);
+	
 	@Query(value="SELECT MAX(sort_order) FROM bullpen_song WHERE user_id = ?1", nativeQuery=true)
 	public int getMaxSortOrderByUserId(int userId);
 }
