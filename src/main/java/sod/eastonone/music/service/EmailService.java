d0bcce7cf00c322985b4a8b6e6253a91eb3896cd
dbrown725 <davidbrown772255@gmail.com>
2023-05-20 16:11:27 -0400

    added video thumbnail image to emails

--- src/main/java/sod/eastonone/music/service/EmailService.java
+++ src/main/java/sod/eastonone/music/service/EmailService.java
@@ -37,16 +37,30 @@ public class EmailService {
 				msgLine = "<div>" + message + "</div><br/>";
 			}
 
+			String videoId = "";
+			String videoUrl = sodSong.getYoutubeUrl();
+			if (videoUrl.startsWith("https://www.youtube.com") && videoUrl.length() >= 43) {
+				videoId = videoUrl.substring(32, 43);
+			} else if (videoUrl.startsWith("https://youtu.be") && videoUrl.length() >= 28) {
+				videoId = videoUrl.substring(17, 28);
+			}
+
 			String emailText = "<html><body>"
 					+ "<div style=\"background-color:#3880FF; color:#FFFFFF; font-size:46px; text-align: center; \">Song of the Day</div><br/>"
+					+ "<div align=\"left\" valign=\"middle\" "
+					+ "style=\"width: 300px; height: 168px; background:url('https://i.ytimg.com/vi/" + videoId + "/hqdefault.jpg'); "
+					+ "background-size:  cover; background-position: center; margin-bottom: 20px\">"
+					+ "<a href=\"" + videoUrl + "\"" + "target=\"_blank\""
+							+ "style=\"display:block;width:100%;text-decoration:none;height:168px;\"></a>"
+					+ "</div>"
 					+ "<div>Submitter: " + sodSong.getUser().getFirstName() + " " + sodSong.getUser().getLastName()
-					+ "</div><br/>" + msgLine + "<div><a href=\"" + sodSong.getYoutubeUrl() + "\""
+					+ "</div><br/>" + msgLine + "<div><a href=\"" + videoUrl + "\""
 					+ "target=\"_blank\">" + sodSong.getTitle() + "</a></div><br/>" + "<div> Band: "
 					+ sodSong.getActualBandName() + "</div><br/>" + "<div> Song: " + sodSong.getActualSongName()
 					+ "</div><br/>" + "<div> Playlist: <a href=\""
-					+ "<PLACEHOLDER_PLAYLIST>" + "\""
+					+ "https://www.youtube.com/playlist?list=PLPFWSmJg6BGh7X7DGsWLdWO-Qt27460De" + "\""
 					+ "target=\"_blank\">" + sodSong.getYoutubePlaylist() + "</a>" + "</div><br/>" + "<div> Visit the "
-					+ "<a href=<PLACEHOLDER_URL>"
+					+ "<a href=\"" + "http://songofthedaymusic.com" + "\""
 					+ "target=\"_blank\">Song of the day</a> website to submit your song." + "</div><br/>"
 					+ "</body></html>";
 
